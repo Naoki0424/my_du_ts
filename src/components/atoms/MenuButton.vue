0243fcd338a171c56d2a215c00fb942ba8e49924
Naoki Tsunoda <Tn04240709@icloud.com>
2022-03-08 23:23:12 +0900

    #1 メニュー作成完了

---
+++ src/components/atoms/MenuButton.vue
@@ -0,0 +1,36 @@
+<template>
+  <button
+    class="nav-link"
+    :class="{
+      'active': active
+    }"
+    data-bs-toggle="pill"
+    :data-bs-target="'#' + targetView"
+    type="button"
+    role="tab"
+  >
+    {{buttonText}}
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "MenuButton",
+  props: {
+    active: {
+      type: Boolean,
+      default: false
+    },
+    targetView: {
+        type: String,
+        required: true
+    },
+    buttonText: {
+        type: String,
+        default: "Default"
+    }
+  }
+});
+</script>
---
+++ src/components/templates/Menu.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="d-flex align-items-start">
+    <div
+      class="nav flex-column nav-pills me-3"
+      id="v-pills-tab"
+      role="tablist"
+      aria-orientation="vertical"
+    >
+      <menu-button :active=true :targetView="'pandas'" :buttonText="'Pandas'" />
+      <menu-button :active=false :targetView="'curl'" :buttonText="'Curl'" />
+    </div>
+    <div class="tab-content" id="tabContent">
+      <div
+        class="tab-pane fade show active"
+        id="pandas"
+        role="tabpanel"
+      >
+      Pandas
+      </div>
+      <div
+        class="tab-pane fade show"
+        id="curl"
+        role="tabpanel"
+      >
+      Curl
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import MenuButton from "@/components/atoms/MenuButton.vue";
+
+export default defineComponent({
+  name: "Menu",
+  components: {
+    MenuButton,
+  },
+});
+</script>
